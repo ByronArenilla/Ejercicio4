a6008872e0dd3f6329f28324ff36e66880737666
Byron Arenilla <barenilla@unal.edu.co>
2024-03-01 17:14:27 -0500

    Primer Commit

---
+++ src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,17 @@
+
+package ejercicio4;
+
+public class Ejercicio4 {
+
+
+    public static void main(String[] args) {
+        double numero, cuadrado, cubo;
+        numero = 2;
+        cuadrado = Math.pow(numero, 2);
+        cubo = Math.pow(numero, 3);
+        System.out.println("El numero es: "+ numero);
+        System.out.println("El cuadrado es: "+ cuadrado);
+        System.out.println("El cubo es: " + cubo);
+    }
+
+}
